87da1d3b327f3729119de4d9f33630b358d60fd5
johanna-codes <johacarvajalg@gmail.com>
2015-07-31 18:39:31 +1000

    NN LogEucl

--- ucf_sports/dim_14/classification/1-NN/ucf-NN.cpp
+++ ucf_sports/dim_14/classification/1-NN/ucf-NN.cpp
@@ -50,7 +50,7 @@ main(int argc, char** argv)
   
   //Cross Validation
     cv_classify_NN CV_onesegment(path, path_dataset, actionNames, scale_factor, shift,  dim);
-//     CV_onesegment.logEucl();
+    CV_onesegment.logEucl();
 //     CV_onesegment.SteinDiv();
 //     CV_onesegment.proj_grass(p);
 
