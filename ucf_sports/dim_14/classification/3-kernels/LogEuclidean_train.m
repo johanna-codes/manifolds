4bc24885b48538a2cb2e4763c3c3b3bf60ec0f00
johanna-codes <johacarvajalg@gmail.com>
2015-08-04 17:40:38 +1000

    Finding Bug

--- ucf_sports/dim_14/classification/3-kernels/LogEuclidean_train.m
+++ ucf_sports/dim_14/classification/3-kernels/LogEuclidean_train.m
@@ -44,7 +44,7 @@ for video_ts= 1: n_videos
         end
         
         
-        size(K_train)
+        size(X_train)
         K_train = compute_kernel_svm(X_train,X_train, RIEMANNIAN_KERNEL, sigma);
         model = svmtrain(labels_train, [[1:size(K_train,1)]' K_train], '-t 4 -q ');
         %Borrame Funciona Bien
