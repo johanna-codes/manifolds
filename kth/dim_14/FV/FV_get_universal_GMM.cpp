8daaf66f9ad079bd2338e80756203992da0067b9
johanna-codes <johacarvajalg@gmail.com>
2015-08-13 13:44:04 +1000

    Get Universal GMM for kth sc1

--- kth/dim_14/FV/FV_get_universal_GMM.cpp
+++ kth/dim_14/FV/FV_get_universal_GMM.cpp
@@ -37,7 +37,7 @@ int
 main(int argc, char** argv)
 {
   
-   if(argc < 3 )
+  if(argc < 3 )
   {
     cout << "usage: " << argv[0] << " scale_factor " << " shift_factor " << endl;
     return -1;
@@ -60,41 +60,80 @@ main(int argc, char** argv)
   
   
   
-  //mat uni_features;
-  mat mat_features;
+  mat uni_features;
+  
   
   
   for (int pe = 0; pe< n_peo; ++pe)
+  {
+    
+    mat mat_features_tmp;
+    mat mat_features;
+    for (int act=0; act<n_actions; ++act)
+    {
+      mat mat_features_video_i;  
+      std::stringstream load_folder;
+      std::stringstream load_feat_video_i;	  
+      load_folder << path << "dim_" << dim <<"/features/kth-features_dim" << dim <<  "/sc" << sc << "/scale" << scale_factor << "-shift"<< shift ;
+      load_feat_video_i << load_folder.str() << "/" << all_people (pe) << "_" << actions(act) << "_dim" << dim  << ".h5";
+      mat_features_video_i.load( load_feat_video_i.str(), hdf5_binary );
+      //getchar();
+      
+      if ( mat_features_video_i.n_cols>0 )
       {
-	for (int act=0; act<n_actions; ++act)
-	{
-	  mat mat_features_video_i;  
-	  std::stringstream load_folder;
-	  std::stringstream load_feat_video_i;	  
-	  load_folder << path << "dim_" << dim <<"/features/kth-features_dim" << dim <<  "/sc" << sc << "/scale" << scale_factor << "-shift"<< shift ;
-	  load_feat_video_i << load_folder.str() << "/" << all_people (pe) << "_" << actions(act) << "_dim" << dim  << ".h5";
-	  mat_features_video_i.load( load_feat_video_i.str(), hdf5_binary );
-	  //getchar();
-	  
-	  if ( mat_features_video_i.n_cols>0 )
-	  {
-	    mat_features	 = join_rows( mat_features, mat_features_video_i );
-	    
-	  }
-	  else 
-	  {
-	    cout << "# vectors = 0 in " << load_feat_video_i.str() << endl;     
-	    
-	  }
-	  
-	}
-	  cout << "r&c "<<  mat_features.n_rows << " & " << mat_features.n_cols << endl;
+	mat_features_tmp	 = join_rows( mat_features_tmp, mat_features_video_i );
+	
+      }
+      else 
+      {
+	cout << "# vectors = 0 in " << load_feat_video_i.str() << endl;     
+	
+      }
+      
+    }
+    cout << "r&c "<<  mat_features_tmp.n_rows << " & " << mat_features_tmp.n_cols << endl;
+    
+    const uword N_max = 10300; //10300x25 = 257.500 ~= 256.000 as per Improved Trajectories Features
+    
+    if (mat_features_tmp.n_cols > N_max)
+    {
+      ivec tmp1 = randi( N_max, distr_param(0,mat_features_tmp.n_cols-1) );
+      ivec tmp2 = unique(tmp1);
+      uvec my_indices = conv_to<uvec>::from(tmp2);
+      mat_features = mat_features_tmp.cols(my_indices);  // extract a subset of the columns
+      
     }
     
+    else
+    {
+      mat_features = mat_features_tmp;
+      
+    }
     
-    cout << "Final r&c "<<  mat_features.n_rows << " & " << mat_features.n_cols << endl;
-
+    cout << "mat_features.n_cols "<< mat_features.n_cols << endl;
     
+    
+    if ( mat_features.n_cols>0 )
+    {
+      uni_features	 = join_rows( uni_features, mat_features );
+      
+    }
+    else 
+    {
+      cout << "# vectors = 0 in uni_features" << endl;
+      
+    }
+    
+    //uni_features =  join_rows( uni_features, mat_features );
+    mat_features_tmp.reset();
+    mat_features.reset();
+    
+  }
+  
+  
+  cout << "Final r&c "<<  uni_features.n_rows << " & " << uni_features.n_cols << endl;
+  
+  
   
   
   
