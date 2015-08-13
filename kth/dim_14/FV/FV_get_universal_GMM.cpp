3ea9eb87c8a9687fe08e076c9be4d5a53fd7b996
johanna-codes <johacarvajalg@gmail.com>
2015-08-13 13:47:42 +1000

    Get Universal GMM for kth sc1

--- kth/dim_14/FV/FV_get_universal_GMM.cpp
+++ kth/dim_14/FV/FV_get_universal_GMM.cpp
@@ -91,7 +91,7 @@ main(int argc, char** argv)
       }
       
     }
-    cout << "r&c "<<  mat_features_tmp.n_rows << " & " << mat_features_tmp.n_cols << endl;
+    //cout << "r&c "<<  mat_features_tmp.n_rows << " & " << mat_features_tmp.n_cols << endl;
     
     const uword N_max = 10300; //10300x25 = 257.500 ~= 256.000 as per Improved Trajectories Features
     
@@ -110,7 +110,7 @@ main(int argc, char** argv)
       
     }
     
-    cout << "mat_features.n_cols "<< mat_features.n_cols << endl;
+    //cout << "mat_features.n_cols "<< mat_features.n_cols << endl;
     
     
     if ( mat_features.n_cols>0 )
@@ -133,7 +133,91 @@ main(int argc, char** argv)
   
   cout << "Final r&c "<<  uni_features.n_rows << " & " << uni_features.n_cols << endl;
   
+  // **************************universal GMM*******************************
   
+   bool is_finite = uni_features.is_finite();
+ 
+    if (!is_finite )
+    {
+      cout << "is_finite?? " << is_finite << endl;
+      cout << uni_features.n_rows << " " << uni_features.n_cols << endl;
+      getchar();
+    
+    }
+  
+  
+  cout << "universal GMM" << endl;
+  gmm_diag gmm_model;
+  gmm_diag bg_model;
+  
+  
+  
+  bool status_em = false;
+  int rep_em=0;
+  
+  
+  int km_iter = 10;
+  int em_iter  = 5;
+  double var_floor = 1e-10;
+  bool print_mode = true;
+  
+  
+  
+  
+  while (!status_em)
+  {
+    bool status_kmeans = false;
+    //int rep_km = 0;
+    
+    while (!status_kmeans)
+    {
+      arma_rng::set_seed_random();
+      
+      status_kmeans = gmm_model.learn(uni_features, N_cent, eucl_dist, random_subset, km_iter, 0, var_floor, print_mode);   //Only Kmeans
+      bg_model = gmm_model;
+      //rep_km++;
+    }
+    
+    
+    status_em = gmm_model.learn(uni_features, N_cent, eucl_dist, keep_existing, 0, em_iter, var_floor, print_mode);   
+    rep_em++;
+    
+    if (rep_em==9)
+    {
+      status_em = true;
+      gmm_model = bg_model;
+      
+    }
+    
+  }
+  
+  
+  cout <<"EM was repeated " << rep_em << endl;
+  
+  std::stringstream tmp_ss5;
+  tmp_ss5 << "/universal_GMM/UniversalGMM_Ng" << N_cent << "_dim" <<dim << "_all_sc"; 
+  cout << "Saving GMM in " << tmp_ss5.str() << endl;
+  gmm_model.save( tmp_ss5.str() );
+  cout << endl;
+  
+  
+  means = gmm_model.means;
+  covs  = gmm_model.dcovs;
+  weights = gmm_model.hefts.t();	
+  
+  //Saving statistics
+  std::stringstream ss_weigths;
+  ss_weigths << "/universal_GMM/weights_Ng" << N_cent << "_dim" << dim << "_all_sc" << ".dat"; 
+  
+  std::stringstream ss_means;
+  ss_means << "/universal_GMM/means_Ng" << N_cent << "_dim" <<dim << "_all_sc" << ".dat"; 
+  
+  std::stringstream ss_covs;
+  ss_covs << "universal_GMM/covs_Ng" << N_cent << "_dim" <<dim << "_all_sc" << ".dat"; 
+  
+  weights.save( ss_weigths.str(), raw_ascii );
+  means.save( ss_means.str(), raw_ascii );
+  covs.save(ss_covs.str(), raw_ascii);
   
   
   
