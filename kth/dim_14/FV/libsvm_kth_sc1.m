c49358bcd10ab1fe27aeb677b453b82d17ebc614
johanna-codes <johacarvajalg@gmail.com>
2015-08-13 16:43:23 +1000

    Train SVM

--- kth/dim_14/FV/libsvm_kth_sc1.m
+++ kth/dim_14/FV/libsvm_kth_sc1.m
@@ -30,7 +30,7 @@ vec_shift = [ -25, -20, -15, -10, -5, 5, 10, 15, 20, 25 ];
 scale_factor = 1;
 
 for i=1:length(vec_shift)
-    show_you = strcat('Getting FVs for ', vec_shift(i) );
+    show_you = strcat('Getting FVs for ', int2str( vec_shift(i) ) );
     disp(show_you);
     FV_kth_all_videos(Ncent, dim, scale_factor, vec_shift(i));
 end
