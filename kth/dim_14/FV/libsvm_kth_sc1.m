259741c22756d9990cf9a03a109de45e66a642df
johanna-codes <johacarvajalg@gmail.com>
2015-08-14 11:43:02 +1000

    Train SVM. Test SVM

--- kth/dim_14/FV/libsvm_kth_sc1.m
+++ kth/dim_14/FV/libsvm_kth_sc1.m
@@ -35,34 +35,34 @@ vec_shift = [ -25, -20, -15, -10, 0,  -5, 5, 10, 15, 20, 25 ];
 % end
 
 %% Training
-scale_factor = 1;
-shift = 0;
-load_sub_path =strcat('./FV_training/scale', int2str(scale_factor), '-shift',  int2str(shift));
-for pe_ts= 1: n_peo
-    X_train = zeros(dim_FV,n_test);
-    labels_train = zeros(n_test,1);
-    k =1;
-    for pe_tr=1: n_peo
-        if pe_tr~=pe_ts
-            for act=1: n_actions
-                %show_you = strcat(all_people(pe_tr),  '_', actions(act));
-                %disp(show_you);
-                name_load_FV = strcat( load_sub_path, '/FV_', all_people(pe_tr),'_',actions(act),'_sc', sc, '_Ng', Ng, '.txt');
-                one_FV = load( char( name_load_FV ) );
-                X_train(:,k) = one_FV;
-                labels_train(k) = act;
-                k=k+1;
-            end
-        end
-    end
-    
-    %size(X_train);
-    data_train = X_train';
-    model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1' ]);
-    save_svm_model =strcat( './svm_models/run_person', int2str(pe_ts), '.mat')
-    save(save_svm_model, 'model');
-    
-end
+% scale_factor = 1;
+% shift = 0;
+% load_sub_path =strcat('./FV_training/scale', int2str(scale_factor), '-shift',  int2str(shift));
+% for pe_ts= 1: n_peo
+%     X_train = zeros(dim_FV,n_test);
+%     labels_train = zeros(n_test,1);
+%     k =1;
+%     for pe_tr=1: n_peo
+%         if pe_tr~=pe_ts
+%             for act=1: n_actions
+%                 %show_you = strcat(all_people(pe_tr),  '_', actions(act));
+%                 %disp(show_you);
+%                 name_load_FV = strcat( load_sub_path, '/FV_', all_people(pe_tr),'_',actions(act),'_sc', sc, '_Ng', Ng, '.txt');
+%                 one_FV = load( char( name_load_FV ) );
+%                 X_train(:,k) = one_FV;
+%                 labels_train(k) = act;
+%                 k=k+1;
+%             end
+%         end
+%     end
+%     
+%     %size(X_train);
+%     data_train = X_train';
+%     model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1' ]);
+%     save_svm_model =strcat( './svm_models/run_person', int2str(pe_ts), '.mat')
+%     save(save_svm_model, 'model');
+%     
+% end
 
 %% Testing
 
@@ -91,6 +91,7 @@ for i=1:length(vec_shift)
             X_train(:,1) = one_FV;
             
             [predicted_label, accuracy, prob_estimates] = svmpredict([act_ts], X_train', model, ['-b 1']);
+            predicted_label
             est_labels(j) = predicted_label;
             j=j+1;
             
