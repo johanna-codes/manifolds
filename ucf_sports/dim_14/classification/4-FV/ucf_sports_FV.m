22fe4a803edc3fe62d154ec5f01fd9fd43e35a18
johanna-codes <johacarvajalg@gmail.com>
2015-08-15 12:26:14 +1000

    Run FV training for UCF_SPORTS

--- ucf_sports/dim_14/classification/4-FV/ucf_sports_FV.m
+++ ucf_sports/dim_14/classification/4-FV/ucf_sports_FV.m
@@ -67,7 +67,7 @@ for video_ts= 1: num_videos
         
         data_train = X_train';
         model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1' ]);
-        save_svm_model = strcat( './svm_models/run_person', int2str(pe_ts), '.mat')
+        save_svm_model = strcat( './svm_models/run_person', int2str(video_ts), '.mat')
         save(save_svm_model, 'model');
     end
 end
