1abef404edb6444f5abca910dbed89c106cbbef6
johanna-codes <johacarvajalg@gmail.com>
2015-08-15 21:34:06 +1000

    Adding GMM for ucf_sports

--- ucf_sports/dim_14/classification/5-GMM/GMM_ucf_sport.cpp
+++ ucf_sports/dim_14/classification/5-GMM/GMM_ucf_sport.cpp
@@ -27,11 +27,11 @@ field<std::string> actions;
 
 inline
 void
-train_ucf(int N_cent,  int dim, int sc);
+train_ucf(field <std::string> action_seq_names, int N_cent,  int dim, int sc);
 
 inline
 float
-test_ucf(int N_cent, int dim, int sc, int scale_factor, int shift );
+test_ucf(field <std::string> action_seq_names, int N_cent, int dim, int sc, int scale_factor, int shift );
 
 
 inline 
@@ -55,46 +55,7 @@ main(int argc, char** argv)
   
   int N_cent = 256; // as per Improved Trajectories Features
   
-  
-  //**********Train the mode**********************
-  train_ucf( N_cent, dim, sc);
-  
-  //   //Test
-  //   vec vec_shift;  
-  //   vec all_acc;
-  //  
-  //   vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
-  //    all_acc.zeros(vec_shift.n_elem);
-  //    
-  //   for (int i=0; i<vec_shift.n_elem; ++i)
-  //   {
-  //   int scale_factor =1;
-  //   int shift = vec_shift(i);
-  //   all_acc(i) = test_ucf( N_cent, dim, sc, scale_factor, shift );
-  //   }
-  //   
-  //   vec_shift.t().print();
-  //   all_acc.t().print();
-  
-  
-  return 0;
-}
-
-
-//*************************************************************
-//***************************Train******************************
-
-inline
-void
-train_ucf(int N_cent, int dim, int sc)
-{
-  actions.load( actionNames ); 
-  
-  int scale_factor =1;
-  int shift = 0;
-  int n_actions = actions.n_rows;
-  
-  int k=0;
+    int k=0;
   field <std::string> action_seq_names (150-1,3);  ///From the dataset description (150)
   
   
@@ -123,21 +84,60 @@ train_ucf(int N_cent, int dim, int sc)
   }
   
   
-  std::stringstream load_folder;
+  //**********Train the mode**********************
+  train_ucf( action_seq_names, N_cent, dim, sc);
   
+  //   //Test
+  //   vec vec_shift;  
+  //   vec all_acc;
+  //  
+  //   vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
+  //    all_acc.zeros(vec_shift.n_elem);
+  //    
+  //   for (int i=0; i<vec_shift.n_elem; ++i)
+  //   {
+  //   int scale_factor =1;
+  //   int shift = vec_shift(i);
+  //   all_acc(i) = test_ucf( N_cent, dim, sc, scale_factor, shift );
+  //   }
+  //   
+  //   vec_shift.t().print();
+  //   all_acc.t().print();
+  
+  
+  return 0;
+}
+
+
+//***********************************************************************
+//*******************************Train***********************************
+
+inline
+void
+train_ucf(field <std::string> action_seq_names, int N_cent, int dim, int sc)
+{
+  actions.load( actionNames ); 
+  
+  int scale_factor =1;
+  int shift = 0;
+  int n_actions = actions.n_rows;
+  
+  std::stringstream load_folder;
   load_folder << path <<"dim_" << dim <<  "/features/features_dim" << dim << "/scale" << scale_factor << "-shift"<< shift;
   
  
   for (int test_i = 0; test_i< action_seq_names.n_rows; ++test_i)
   {
+
+    cout << "Doing for " << test_i << endl;
     
     field<mat> field_all_actions(n_actions);
-
-      for (int train_i = 0; train_i< action_seq_names.n_rows; ++train_i)
-      {
+    
+    for (int train_i = 0; train_i< action_seq_names.n_rows; ++train_i)
+    {
 	std::string action_name = action_seq_names(train_i,0);   
 	std::string folder_n    = action_seq_names(train_i,1);
-	int index_act  = atoi( action_seq_names(test_i,2).c_str() );
+	int index_act  = atoi( action_seq_names(train_i,2).c_str() );
 	
 	if (test_i!=train_i)
 	{
@@ -160,13 +160,13 @@ train_ucf(int N_cent, int dim, int sc)
 	cout << "Final r&c "<<  features_action_i.n_rows << " & " << features_action_i.n_cols << endl;
 	get_gmm (features_action_i, N_cent,  dim,  test_i,  act );
       }
-      
-    getchar();
+
   }
   
 }
 
-// **************************GMM*******************************
+// ************************************GMM********************************************
+// ***********************************************************************************
 
 inline 
 void
@@ -238,3 +238,91 @@ get_gmm (mat& features_action_i, int N_cent, int dim, int test_i, int act )
   cout << endl;
   
 }
+
+
+inline
+float
+test_ucf(field <std::string> action_seq_names, int N_cent, int dim, int sc, int scale_factor, int shift )
+{
+  actions.load( actionNames ); 
+  
+  int n_actions = actions.n_rows;
+
+  
+  int n_test = action_seq_names.n_rows;
+  
+  vec real_labels;
+  vec est_labels;
+  
+  real_labels.zeros(n_test);
+  est_labels.zeros(n_test);
+  
+  float acc;
+  acc = 0;
+  int k = 0;
+  
+  std::stringstream load_folder;
+  load_folder << path <<"dim_" << dim <<  "/features/features_dim" << dim << "/scale" << scale_factor << "-shift"<< shift;
+  
+  
+  
+  for (int test_i = 0; test_i< action_seq_names.n_rows; ++test_i)
+  {
+    std::string action_name = action_seq_names(test_i,0);   
+    std::string folder_n    = action_seq_names(test_i,1);
+    int index_act  = atoi( action_seq_names(test_i,2).c_str() );
+    
+     std::stringstream load_feat_video_i;
+     load_feat_video_i   << load_folder.str() << "/"     << action_name << "_" << folder_n << "_dim" << dim  << ".h5";
+     
+     mat mat_features_video_i;
+     mat_features_video_i.load( load_feat_video_i.str() , hdf5_binary );
+    
+      
+      vec likelihood_actions =  get_loglikelihoods( mat_features_video_i, test_i, N_cent, dim);
+      
+      uword est_label_video_i;
+      likelihood_actions.max(est_label_video_i);
+      
+      real_labels(k)= index_act;
+      est_labels(k) = est_label_video_i;
+      ++k;
+      
+      if (est_label_video_i == act)
+      {
+	acc++;  
+      }
+      //cout << "Real label is " <<  act << " and it was classified as " << est_label_video_i << endl;
+
+
+  }
+  
+  cout << "Performance for shift: " << shift << " = " << acc*100/n_test << " %" << endl;
+  
+  return acc*100/n_test;
+  
+}
+
+inline
+vec
+get_loglikelihoods(mat &mat_features, int run, int N_cent, int dim)
+{
+  vec likelihood_actions(actions.n_rows);
+  
+  for (uword act_tr=0; act_tr < actions.n_rows; ++act_tr)
+  {
+    gmm_diag gmm_model;
+    
+    std::stringstream tmp_ss5;
+    tmp_ss5 << "./GMM_models/run" << test_i << "_" << actions(act) <<  "_GMM_Ng" << N_cent << "_dim" <<dim;    //cout << "Loading GMM in " << tmp_ss5.str() << endl;
+    gmm_model.load( tmp_ss5.str() );
+    
+    likelihood_actions (act_tr) = gmm_model.avg_log_p(mat_features);
+    
+  }
+
+  return likelihood_actions;
+  
+}
+
+
