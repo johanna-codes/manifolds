d44ff36e028dbe3d5c63c8d15ea484a2dbcb7b73
johanna-codes <johacarvajalg@gmail.com>
2015-08-15 21:39:33 +1000

    Adding GMM for ucf_sports

--- ucf_sports/dim_14/classification/5-GMM/GMM_ucf_sport.cpp
+++ ucf_sports/dim_14/classification/5-GMM/GMM_ucf_sport.cpp
@@ -159,6 +159,7 @@ train_ucf(field <std::string> action_seq_names, int N_cent, int dim, int sc)
 	features_action_i = field_all_actions(act);
 	field_all_actions(act).reset();
 	cout << "Final r&c "<<  features_action_i.n_rows << " & " << features_action_i.n_cols << endl;
+	getchar();
 	get_gmm (features_action_i, N_cent,  dim,  test_i,  act );
       }
 
