66d0ea4a9f8422fb9ddeeb1729141f85f38fe373
johanna-codes <johacarvajalg@gmail.com>
2015-08-15 12:23:49 +1000

    Run FV training for UCF_SPORTS

--- ucf_sports/dim_14/classification/4-FV/ucf_sports_FV.m
+++ ucf_sports/dim_14/classification/4-FV/ucf_sports_FV.m
@@ -24,42 +24,57 @@ num_videos = 150; %From the dataset description (150).
 
 %% Get FV. Run Just once
 
- scale_factor = 1;
- for i=1:length(vec_shift)
-     show_you = strcat('Getting FVs for ', int2str( vec_shift(i) ) );
-     disp(show_you);
-     FV_ucf_sports_all_videos( path, Ncent, dim, scale_factor, vec_shift(i), num_videos, action_seq_names );
- end
+%  scale_factor = 1;
+%  for i=1:length(vec_shift)
+%      show_you = strcat('Getting FVs for ', int2str( vec_shift(i) ) );
+%      disp(show_you);
+%      FV_ucf_sports_all_videos( path, Ncent, dim, scale_factor, vec_shift(i), num_videos, action_seq_names );
+%  end
 
 %% Training TODO
-% scale_factor = 1;
-% shift = 0;
-% load_sub_path =strcat('./FV_training/scale', int2str(scale_factor), '-shift',  int2str(shift));
-% for pe_ts= 1: n_peo
-%     X_train = zeros(dim_FV,n_test);
-%     labels_train = zeros(n_test,1);
-%     k =1;
-%     for pe_tr=1: n_peo
-%         if pe_tr~=pe_ts
-%             for act=1: n_actions
-%                 %show_you = strcat(all_people(pe_tr),  '_', actions(act));
-%                 %disp(show_you);
-%                 name_load_FV = strcat( load_sub_path, '/FV_', all_people(pe_tr),'_',actions(act),'_sc', sc, '_Ng', Ng, '.txt');
-%                 one_FV = load( char( name_load_FV ) );
-%                 X_train(:,k) = one_FV;
-%                 labels_train(k) = act;
-%                 k=k+1;
-%             end
-%         end
-%     end
-%     
-%     %size(X_train);
-%     data_train = X_train';
-%     model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1' ]);
-%     save_svm_model =strcat( './svm_models/run_person', int2str(pe_ts), '.mat')
-%     save(save_svm_model, 'model');
-%     
-% end
+scale_factor = 1;
+shift = 0;
+n_test =  num_videos - 2; %Problem with Run-Side_001_dim14
+
+load_sub_path =strcat('./FV_training/scale', int2str(scale_factor), '-shift',  int2str(shift));
+
+for video_ts= 1: num_videos
+    X_train = zeros(dim_FV,n_test);
+    labels_train = zeros(n_test,1);
+    k =1;
+    
+    action_name = action_seq_names(video_ts,1);
+    folder_n    = action_seq_names(video_ts,2);
+    
+    
+    if (~(strcmp(action_name,'Run-Side') && strcmp(folder_n,'001')))
+        %ts = [action_name,'_',folder_n];
+        %disp(ts);
+        for video_tr=1: num_videos
+            if (video_tr~=video_ts)
+                action_name_tr = action_seq_names(video_tr,1);
+                folder_n_tr    = action_seq_names(video_tr,2);
+                act_tr  =  str2double( action_seq_names(video_tr,3) );
+                if (~(strcmp(action_name_tr, 'Run-Side') && strcmp(folder_n_tr,'001')))
+                    name_load_FV = strcat( load_sub_path, '/FV_', action_name, '_', folder_n, '_Ng', Ng, '.txt');
+                    one_FV = load( char( name_load_FV ) );
+                    X_train(:,k) = one_FV;
+                    labels_train(k) = act_tr;
+                    k=k+1;
+                end
+            end
+        end
+        
+        data_train = X_train';
+        model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1' ]);
+        save_svm_model = strcat( './svm_models/run_person', int2str(pe_ts), '.mat')
+        save(save_svm_model, 'model');
+    end
+end
+
+
+
+
 
 %% Testing TODO
 
