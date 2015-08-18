168d533dc0d5bf0b928f23da3c0e702415a635f4
johanna-codes <johacarvajalg@gmail.com>
2015-08-18 10:35:14 +1000

    Run KTH_PM dim12

--- kth/dim_12_no_xy/classification/kth-classification-onematrix-video.cpp
+++ kth/dim_12_no_xy/classification/kth-classification-onematrix-video.cpp
@@ -89,7 +89,7 @@ main(int argc, char** argv)
   int scale_factor = 1;
   
   vec vec_shift;
-  vec_shift << -25 << -20 << -15 << -10 << -5 << 5 << 10 << 15 << 20 << 25 << endr;
+  vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
   
   vec vec_pm_shifts = zeros(vec_shift.n_elem);
   
