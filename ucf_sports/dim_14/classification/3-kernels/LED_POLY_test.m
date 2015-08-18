d9f735b7bfc8780e4ebd3bba5badb9c7bc3b240d
johanna-codes <johacarvajalg@gmail.com>
2015-08-18 22:00:16 +1000

    Test Kernel LED-POLY for ucf_sports

--- ucf_sports/dim_14/classification/3-kernels/LED_POLY_test.m
+++ ucf_sports/dim_14/classification/3-kernels/LED_POLY_test.m
@@ -39,7 +39,7 @@ j=1;
 
       end
       
-     save_labels = strcat('./svm_results/LED_POLY_scale', int2str(scale_factor), '-shift', int2str(shift),'-n',num2str(n),'.mat' );     
+     save_labels = strcat('./svm_results_LED_POLY/LED_POLY_scale', int2str(scale_factor), '-shift', int2str(shift),'-n',num2str(n),'.mat' );     
      save(save_labels, 'est_labels', 'real_labels', 'sigma');
   
   end
