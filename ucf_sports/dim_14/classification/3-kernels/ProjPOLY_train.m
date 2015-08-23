e251754cff1ff2344202e023bdf35e2bb7fda0d8
johanna-codes <johacarvajalg@gmail.com>
2015-08-23 16:38:40 +1000

    Run ProjPoly

--- ucf_sports/dim_14/classification/3-kernels/ProjPOLY_train.m
+++ ucf_sports/dim_14/classification/3-kernels/ProjPOLY_train.m
@@ -22,8 +22,8 @@ parfor video_ts= 1: n_videos
     
     
     if (~(strcmp(action_name,'Run-Side') && strcmp(folder_n,'001')))
-        %ts = [action_name,'_',folder_n];
-        %disp(ts);
+        ts = [action_name,'_',folder_n];
+        disp(ts);
         for video_tr=1: n_videos
             if (video_tr~=video_ts)
                 action_name_tr = action_seq_names(video_tr,1);
@@ -47,7 +47,7 @@ parfor video_ts= 1: n_videos
         model = svmtrain(labels_train, [[1:size(K_train,1)]' K_train], '-t 4 -q ');
         %Borrame Funciona Bien
         [predict_label, accuracy, dec_values] = svmpredict(labels_train,[[1:size(K_train,1)]' K_train], model);
-        %display(accuracy');
+        display(accuracy');
         
         acc = [acc accuracy(1)];
         save_svm_model =strcat( './svm_models_ProjectionPOLY/projPOLY_run_', int2str(video_ts), '_degree', num2str(p),'.mat');
