480aaf308d7efe57ff811abc352da1ee98648a2b
johanna-codes <johacarvajalg@gmail.com>
2015-09-20 18:21:34 +1000

    Adding GMM for UT-Tower

--- ut_tower/classification/5-GMM/GMM_ut_tower.cpp
+++ ut_tower/classification/5-GMM/GMM_ut_tower.cpp
@@ -138,12 +138,12 @@ train_ut(mat action_seq_names, int N_cent, int dim)
       
       if (test_i!=train_i)
       {
-	cout << load_folder.str()  << endl;
+	//cout << load_folder.str()  << endl;
 	//cout << action_name << "_" << folder_n << " ";
 	std::stringstream load_feat_video_i;
 	load_feat_video_i   << load_folder.str() << "/" << actions(actID)  << "_seq" <<  seqID << ".h5";
 	//getchar();
-	
+	cout << actions(actID)  << "_seq" <<  seqID << ".h5" << endl;;
 	mat mat_features_video_i;
 	mat_features_video_i.load( load_feat_video_i.str() , hdf5_binary );
 	cout << "mat_features_video_i r&c "<<  mat_features_video_i.n_rows << " & " << mat_features_video_i.n_cols << endl;
