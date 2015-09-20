3fd07aab95b26b86f8e2a6a8f1b02447e02da556
johanna-codes <johacarvajalg@gmail.com>
2015-09-20 14:57:22 +1000

    Run Cov Matrices UT-Tower: All shifts and Scales

--- ut_tower/cov_matrices/ut-tower-cov-mat.cpp
+++ ut_tower/cov_matrices/ut-tower-cov-mat.cpp
@@ -30,16 +30,47 @@ int
 main(int argc, char** argv)
 {
   
-  
-  int shift = 0;
-  float scale_factor = 1;
-  
   int dim = 14; 
-  cov_mat get_cov_seg(path, actionNames, scale_factor, shift, dim);
-  get_cov_seg.calculate_one_per_video(  );
+  int shift = 0;
+  vec scale_vec;
+  scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95  << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;
+  
+  vec vec_shift;
+  vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
+  
+  
+  for (int j=0; j<vec_shift.n_elem; ++j)
+  {
+    float scale_factor = 1;
+    int shift = vec_shift(j);
+    cout << "Scale & Shift: " << scale_factor << " & " << shift << endl;
     
+    cov_mat get_cov_seg(path, actionNames, scale_factor, shift, dim);
+    get_cov_seg.calculate_one_per_video(  );
+  }
+  
+  for (int i=0; i<scale_vec.n_elem; ++i)
+  {
+    float scale_factor = scale_vec(i);
+    int shift = 0;
+    cout << "Scale & Shift: " << scale_factor << " & " << shift << endl;
+    //float scale_factor = 1;
+    //cout << "scale_factor " << scale_factor << endl;
     
-
+    cov_mat get_cov_seg(path, actionNames, scale_factor, shift, dim);
+    get_cov_seg.calculate_one_per_video(  );
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   return 0;
   
 }
