58412e1ff4873e722ddcbf09c6f2187984b2d825
johanna-codes <johacarvajalg@gmail.com>
2015-09-20 15:24:10 +1000

    Run Grassman Points UT-Tower: All shifts and Scales

--- ut_tower/grass_points/main-grass-points.cpp
+++ ut_tower/grass_points/main-grass-points.cpp
@@ -32,24 +32,45 @@ main(int argc, char** argv)
 {
   
   int shift = 0;
+  int dim = 14;
+  
   vec scale_vec;
-  scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95  << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;  
-  int dim = 14; 
+  scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95  << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;
+  
+  vec vec_shift;
+  vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
   
   
- //for (int i=0; i<scale_vec.n_elem; ++i)
- //{
+  
+  for (int j=0; j<vec_shift.n_elem; ++j)
+  {
     for (int p=1; p<= dim; ++p)
     {
-      
-      //float scale_factor = scale_vec(i);
-      float scale_factor = 1 ;
-      cout << "Gp for scale_factor " << scale_factor << " &  p= " << p << endl;
-      
+      float scale_factor = 1;
+      int shift = vec_shift(j);
+      //cout << "Scale & Shift: " << scale_factor << " & " << shift << endl;
+           
+      cout << "Gp for scale_factor " << scale_factor << " &  shift= " << shift  << " &  p= " << p << endl;
       grass_points get_gp(path, actionNames, scale_factor, shift, dim, p);
       get_gp.calculate_onepervideo( );
     }
-  //}
+  }
+  
+  for (int i=0; i<scale_vec.n_elem; ++i)
+  {
+    for (int p=1; p<= dim; ++p)
+    {
+      float scale_factor = scale_vec(i);
+      int shift = 0;
+      //cout << "Scale & Shift: " << scale_factor << " & " << shift << endl;
+      
+      cout << "Gp for scale_factor " << scale_factor << " &  shift= " << shift  << " &  p= " << p << endl;
+      grass_points get_gp(path, actionNames, scale_factor, shift, dim, p);
+      get_gp.calculate_onepervideo( );
+    }
+  }
+  
+  
   
   return 0;
   
