6da7c6a2d5f5c5e306cec939736c7bc14fe907d8
johanna-codes <johacarvajalg@gmail.com>
2015-09-23 09:16:12 +1000

    Test svm + Kernel LED-POLY

--- ut_tower/classification/3-kernels/main_train_test.m
+++ ut_tower/classification/3-kernels/main_train_test.m
@@ -29,22 +29,22 @@ num_videos = 108; %From the dataset description (108).
 %      %ACC_train(i,:) = acc;
 %  end
 %
-% display('Testing svm + Kernel LED-POLY');
-% n=1:dim;
-% test_acc = zeros(length(n),1);
-% scale = 1;
-% shift = 0;
-%
-% for i=1:length(n)
-%     in_n = n(i);
-%     X = sprintf('n = %d ', in_n);
-%     acc = LED_POLY_test(path,action_seq_names,scale, shift, dim, in_n, num_videos);
-%     test_acc(i) = acc;
-%     test_acc'
-% end
-%
-% save_results =strcat( 'LED_POLY_all_n_performance.mat');
-% save(save_results, 'test_acc', 'n', 'dim', 'scale', 'shift');
+display('Testing svm + Kernel LED-POLY');
+n=1:dim;
+test_acc = zeros(length(n),1);
+scale = 1;
+shift = 0;
+
+for i=1:length(n)
+    in_n = n(i);
+    X = sprintf('n = %d ', in_n);
+    acc = LED_POLY_test(path,action_seq_names,scale, shift, dim, in_n, num_videos);
+    test_acc(i) = acc;
+    test_acc'
+end
+
+save_results =strcat( 'LED_POLY_all_n_performance.mat');
+save(save_results, 'test_acc', 'n', 'dim', 'scale', 'shift');
 
 %% Kernel LED-RBF.
 
@@ -109,27 +109,27 @@ num_videos = 108; %From the dataset description (108).
 % end
 
 
-display('Testing svm + Projection RBF Kernel');
-delta = -14:2:20;
-p = 1:dim;
-test_acc = zeros( length(delta),1 );
-all_p = cell(length(p),1);
-scale = 1;
-shift = 0;
-for j=1:length(p)
-    in_p = p(j);
-    for i=1:length(delta )
-        in_delta = delta(i);
-        X = sprintf('p= %d and delta = %d ', in_p, in_delta);
-        disp(X);
-        acc = ProjectionRBF_test(path, action_seq_names, scale, shift, in_delta, dim, in_p, num_videos);
-        test_acc(i) = acc;
-    end
-    all_p{j} = test_acc;
-end
-
-save_results =strcat( 'projRBF_all_p_delta_performance.mat');
-save(save_results, 'all_p', 'delta', 'p', 'dim', 'scale', 'shift');
+% display('Testing svm + Projection RBF Kernel');
+% delta = -14:2:20;
+% p = 1:dim;
+% test_acc = zeros( length(delta),1 );
+% all_p = cell(length(p),1);
+% scale = 1;
+% shift = 0;
+% for j=1:length(p)
+%     in_p = p(j);
+%     for i=1:length(delta )
+%         in_delta = delta(i);
+%         X = sprintf('p= %d and delta = %d ', in_p, in_delta);
+%         disp(X);
+%         acc = ProjectionRBF_test(path, action_seq_names, scale, shift, in_delta, dim, in_p, num_videos);
+%         test_acc(i) = acc;
+%     end
+%     all_p{j} = test_acc;
+% end
+% 
+% save_results =strcat( 'projRBF_all_p_delta_performance.mat');
+% save(save_results, 'all_p', 'delta', 'p', 'dim', 'scale', 'shift');
 
 
 
