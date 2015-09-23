f5fd80e6ded5b355c48f7cda6ff033a0df492632
johanna-codes <johacarvajalg@gmail.com>
2015-09-23 11:58:54 +1000

    Run Led-Poly for KTH

--- kth/dim_14/kernels/kth_train_LED_POLY.m
+++ kth/dim_14/kernels/kth_train_LED_POLY.m
@@ -31,6 +31,7 @@ for pe_ts= 1: n_peo
                 %name_display = strcat('cov_', all_people(pe_tr), '_', actions(act), '_dim', int2str(dim), '.h5');                
                 %disp(name_display)
                 hinfo = hdf5info( char(name_load_cov) );
+                size(one_video)
                 one_video = hdf5read(hinfo.GroupHierarchy.Datasets(1));
                 X_train(:,:,k) = one_video;
                 labels_train(k) = act;
