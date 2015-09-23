fbf973fff45027ba2813c21ca51f8cd154b7ab0e
johanna-codes <johacarvajalg@gmail.com>
2015-09-24 08:22:47 +1000

    Run Shifts and Scales

--- kth/dim_14/kernels/Shifts_Scales.m
+++ kth/dim_14/kernels/Shifts_Scales.m
@@ -12,35 +12,35 @@ dim =14;
 
 
 %% Kernel LED-Poly: KTH
-% display('Testing svm + Kernel LED-POLY');
-% vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
-% 
-% %n=1:dim;
-% best_n = 17;
-% all_acc_shifts = zeros( length(vec_shift), 1); 
-% scale=1;
-%  
-%  for i=1:length(vec_shift)
-%      shift = vec_shift(i);
-%      sprintf('shift = %d ', shift)
-%      acc = kth_test_LED_POLY(path,scale, shift, dim, best_n);
-%      all_acc_shifts(i) = acc;  
-%      all_acc_shifts'
-%     
-%  end
+display('Testing svm + Kernel LED-POLY. Shifting Videos');
+vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
+
+%n=1:dim;
+best_n = 12;
+all_acc_shifts = zeros( length(vec_shift), 1); 
+scale=1;
+ 
+ for i=1:length(vec_shift)
+     shift = vec_shift(i);
+     sprintf('shift = %d ', shift)
+     acc = kth_test_LED_POLY(path,scale, shift, dim, best_n);
+     all_acc_shifts(i) = acc;  
+     all_acc_shifts'
+    
+ end
  
  
 display('Testing svm + Kernel LED-POLY. Varying Scale');
 vec_scale = [0.75 0.80 0.85 0.90 0.95 1 1.05 1.10  1.15 1.20 1.25];
-best_n = 17;
-all_acc_shifts = zeros( length(vec_scale), 1); 
+best_n = 12;
+all_acc_scales = zeros( length(vec_scale), 1); 
 shift = 0;
  
  for i=1:length(vec_scale)
-     scale=vec_scale(i);;
+     scale=vec_scale(i);
      sprintf('scale = %d ', scale)
      acc = kth_test_LED_POLY(path,scale, shift, dim, best_n);
-     all_acc_shifts(i) = acc;  
-     all_acc_shifts'
+     all_acc_scales(i) = acc;  
+     all_acc_scales'
     
  end
