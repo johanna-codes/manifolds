6c0a8d9d46f214fa9a6f2076539bb39c1f75736d
johanna-codes <johacarvajalg@gmail.com>
2015-09-23 10:45:00 +1000

    Run Led-Poly for KTH. Varying the scales

--- kth/dim_14/kernels/Shifts.m
+++ kth/dim_14/kernels/Shifts.m
@@ -10,20 +10,35 @@ dim =14;
 
  %%%%%%%%%%%%%%%%%%%%   Riemannian Kernels %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
- 
- %% Kernel LED-Poly: KTH
 
-
- display('Testing svm + Kernel LED-POLY');
- %n=1:dim;
- best_n = 17;
- all_acc_shifts = zeros( length(vec_shift), 1); 
- scale=1;
+%% Kernel LED-Poly: KTH
+% display('Testing svm + Kernel LED-POLY');
+% vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
+% 
+% %n=1:dim;
+% best_n = 17;
+% all_acc_shifts = zeros( length(vec_shift), 1); 
+% scale=1;
+%  
+%  for i=1:length(vec_shift)
+%      shift = vec_shift(i);
+%      sprintf('shift = %d ', shift)
+%      acc = kth_test_LED_POLY(path,scale, shift, dim, best_n);
+%      all_acc_shifts(i) = acc;  
+%      all_acc_shifts'
+%     
+%  end
  
- for i=1:length(vec_shift)
-     shift = vec_shift(i);
-     sprintf('shift = %d ', shift)
+ 
+display('Testing svm + Kernel LED-POLY. Varying Scale');
+vec_scale = [0.75 0.80 0.85 0.90 0.95 1 1.05 1.10  1.15 1.20 1.25];
+best_n = 17;
+all_acc_shifts = zeros( length(vec_scale), 1); 
+shift = 0;
+ 
+ for i=1:length(vec_scale)
+     scale=vec_scale(i);;
+     sprintf('scale = %d ', scale)
      acc = kth_test_LED_POLY(path,scale, shift, dim, best_n);
      all_acc_shifts(i) = acc;  
      all_acc_shifts'
