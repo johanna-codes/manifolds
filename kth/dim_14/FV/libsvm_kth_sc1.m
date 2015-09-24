f76809de837dc71c1a669800739feca7a30633c1
johanna-codes <johacarvajalg@gmail.com>
2015-09-24 12:35:42 +1000

    Run FV sclaes for Kth

--- kth/dim_14/FV/libsvm_kth_sc1.m
+++ kth/dim_14/FV/libsvm_kth_sc1.m
@@ -22,7 +22,7 @@ n_test = (n_peo-1)*n_actions;
 
 
 dim_FV = 2*dim*Ncent;
-vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
+
 
 %% Get FV. Run Just once
 
@@ -64,14 +64,15 @@ vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
 %     
 % end
 
-%% Testing
+%% Testing - Shifts
+%vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
+vec_scale = [0.75 0.80 0.85 0.90 0.95 1 1.05 1.10  1.15 1.20 1.25];
 
-
-all_acc_shifts = zeros( length(vec_shift), 1);
-for i=1:length(vec_shift)
-    scale_factor = 1;
-    shift = vec_shift(i)
-    load_sub_path =strcat('./FV_training/scale', int2str(scale_factor), '-shift',  int2str(shift));
+all_acc_scales = zeros( length(vec_scale), 1);
+for i=1:length(vec_scale)
+    scale_factor = vec_scale(i)
+    shift = 0;
+    load_sub_path =strcat('./FV_training/scale', num2str(scale_factor), '-shift',  int2str(shift));
     acc = 0;
     real_labels = zeros(n_peo*n_actions);
     est_labels  = zeros(n_peo*n_actions);
@@ -103,14 +104,66 @@ for i=1:length(vec_shift)
             
         end
         
-        save_labels = strcat('./svm_results/scale', int2str(scale_factor), '-shift', int2str(shift),'.mat' );
+        save_labels = strcat('./svm_results/scale', num2str(scale_factor), '-shift', int2str(shift),'.mat' );
         save(save_labels, 'est_labels', 'real_labels');
         
     end
     %[acc n_peo n_actions]
     acc = acc*100/(n_peo*n_actions)
-    all_acc_shifts(i) = acc;
+    all_acc_scales(i) = acc;
 end
 
-[vec_shift' all_acc_shifts]
+[vec_scale' all_acc_scales]
+
+%% Testing - Shifts
+% vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
+% 
+% all_acc_shifts = zeros( length(vec_shift), 1);
+% for i=1:length(vec_shift)
+%     scale_factor = 1;
+%     shift = vec_shift(i)
+%     load_sub_path =strcat('./FV_training/scale', int2str(scale_factor), '-shift',  int2str(shift));
+%     acc = 0;
+%     real_labels = zeros(n_peo*n_actions);
+%     est_labels  = zeros(n_peo*n_actions);
+%     
+%     j=1;
+%     for pe_ts= 1: n_peo
+%         
+%         load_svm_model = strcat( './svm_models/run_person', int2str(pe_ts), '.mat');
+%         load(load_svm_model); %loading model and X_train
+%         
+%         for act_ts = 1:n_actions
+%             
+%             %show_you = strcat(all_people(pe_ts),  '_', actions(act_ts));
+%             %disp(show_you);
+%             real_labels(j) = act_ts;
+%             
+%             name_load_FV = strcat( load_sub_path, '/FV_', all_people(pe_ts),'_',actions(act_ts), '_sc', sc, '_Ng', Ng, '.txt');
+%             one_FV = load( char( name_load_FV ) );
+%             X_test(:,1) = one_FV;
+%             
+%             [predicted_label, accuracy, prob_estimates] = svmpredict([act_ts], X_test', model, ['-b 1']);
+%             %predicted_label
+%             est_labels(j) = predicted_label;
+%             j=j+1;
+%             
+%             if predicted_label == act_ts
+%                 acc = acc+1;
+%             end
+%             
+%         end
+%         
+%         save_labels = strcat('./svm_results/scale', int2str(scale_factor), '-shift', int2str(shift),'.mat' );
+%         save(save_labels, 'est_labels', 'real_labels');
+%         
+%     end
+%     %[acc n_peo n_actions]
+%     acc = acc*100/(n_peo*n_actions)
+%     all_acc_shifts(i) = acc;
+% end
+% 
+% [vec_shift' all_acc_shifts]
+
+
 
