5d11f4570a6348dbd1f037027aea19bb4688f467
johanna-codes <johacarvajalg@gmail.com>
2015-10-06 15:56:34 +1000

    Run Projection Metric and Binet-Cauchy for KTH

--- kth/dim_14/classification/grassmann-metrics-impl.hpp
+++ kth/dim_14/classification/grassmann-metrics-impl.hpp
@@ -15,6 +15,8 @@
    svd( cos_theta, X );
    
    vec cos2_theta = square(cos_theta);
+   cout << cos2_theta.n_elem<< endl;
+   getchar();
    double tmp_sum = sum(cos2_theta);   
    double dist = sqrt (p - tmp_sum);
    
