6b44328c9c51d6c4af00773a80d13a07b259790a
johanna-codes <johacarvajalg@gmail.com>
2015-10-06 15:08:47 +1000

    Run Binet-Cauchy for KTH

--- kth/dim_14/classification/kth-classification-onematrix-video.cpp
+++ kth/dim_14/classification/kth-classification-onematrix-video.cpp
@@ -81,24 +81,25 @@ main(int argc, char** argv)
     //   }
     //   
     //   vec_pm.t().print("Projection Metric");
-    //   vec_bc.t().print("Binet-Cauchy");
+    
     
     /// Extra experiments 
     int scale_factor = 1;
     int shift = 0;
-    kth_cv_omp kth_CV_omp_onesegment(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
-    kth_CV_omp_onesegment.SteinDiv();
-    kth_CV_omp_onesegment.logEucl();
+    //kth_cv_omp kth_CV_omp_onesegment(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
+    //kth_CV_omp_onesegment.SteinDiv();
+    //kth_CV_omp_onesegment.logEucl();
     
-    //   vec vec_bc = zeros(dim);
-    //   for (int p=1; p<= dim; ++p)
-    //   {
-      //     cout << "p= " << p << endl;
-    //     kth_cv_omp kth_CV_omp_onesegment(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
-    //     vec_bc(p-1) = kth_CV_omp_onesegment.BC_grass(p);
-    //   }
-    //  
-    
+    vec vec_bc = zeros(dim);
+    for (int p=1; p<= dim; ++p)
+    {
+         cout << "p= " << p << endl;
+         kth_cv_omp kth_CV_omp_onesegment(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
+         vec_bc(p-1) = kth_CV_omp_onesegment.BC_grass(p);
+      
+    }
+       
+       vec_bc.t().print("Binet-Cauchy");
     
     
     //   vec vec_bc = zeros(dim);
