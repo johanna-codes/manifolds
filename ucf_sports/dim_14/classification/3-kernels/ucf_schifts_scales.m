1bbd631fc9be41c89f1d6a312144ed84d57b7432
johanna-codes <johacarvajalg@gmail.com>
2015-10-07 13:25:23 +1000

    Run Shifts Scales. Kernel proj-rbf. UCF

--- ucf_sports/dim_14/classification/3-kernels/ucf_schifts_scales.m
+++ ucf_sports/dim_14/classification/3-kernels/ucf_schifts_scales.m
@@ -15,7 +15,8 @@ action_seq_names = list_video_sequences(dataset_path);
 dim =14;
 num_videos = 150; %From the dataset description (150). Problem with Run-Side_001_dim14
 
-%% Kernel LED-Poly. UCF Sports. Scales
+%% Kernel LED-Poly. 
+% UCF Sports. Scales
 
 display('Testing svm + Kernel LED-POLY');
 
@@ -32,7 +33,7 @@ for i=1:length(vec_scale)
     test_acc'
 end
 
-%% Kernel LED-Poly. UCF Sports. Shifts
+% UCF Sports. Shifts
 
 % display('Testing svm + Kernel LED-POLY');
 % 
@@ -50,4 +51,40 @@ end
 % end
 
 
+%% Kernel proj-rbf. 
+%UCF Sports. Scales
+
+display('Testing svm + Kernel LED-POLY');
+
+shift  = 0;
+best_p = 4;
+best_delta = 8;
+vec_scale = [0.75 0.80 0.85 0.90 0.95 1 1.05 1.10  1.15 1.20 1.25];
+test_acc = zeros(length(vec_scale),1);
+
+for i=1:length(vec_scale)
+    scale = vec_scale(i);
+    sprintf('scale = %d ', scale)
+    acc = ProjectionRBF_test(path, action_seq_names, scale, shift, best_delta, dim, best_p, num_videos);
+    test_acc(i) = acc;
+    test_acc'
+end
+
+% UCF Sports. Shifts
+
+% display('Testing svm + Kernel LED-POLY');
+% 
+% scale  = 1;
+% best_p = 4;
+% best_delta = 8;
+% vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
+% test_acc = zeros(length(vec_shift),1);
+% 
+% for i=1:length(vec_shift)
+%     shift = vec_shift(i);
+%     sprintf('shift = %d ', shift)
+%     acc = ProjectionRBF_test(path, action_seq_names, scale, shift,  best_delta, dim, best_p, num_videos);
+%     test_acc(i) = acc;
+%     test_acc'
+% end
 
