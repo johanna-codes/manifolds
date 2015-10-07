8fa03e113414c6a2da79d9c28c493ec59c1ec43b
johanna-codes <johacarvajalg@gmail.com>
2015-10-07 10:37:01 +1000

    Run SHifts for PM UCF

--- ucf_sports/dim_14/classification/1-NN/ucf-NN.cpp
+++ ucf_sports/dim_14/classification/1-NN/ucf-NN.cpp
@@ -54,48 +54,51 @@ main(int argc, char** argv)
   
   
  /// Shifts 
-//   vec vec_shift;
-//   vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
-//   int scale_factor = 1;
-//   int dim = 14; 
-//   
-// 
-//   
-//   vec vec_logEucl   = zeros(vec_shift.n_elem);
-//   
-//   for (int i=0; i< vec_shift.n_elem; ++i)
-//   {
-//     
-//     int shift = vec_shift(i);
-//     cout << "Shift: " << shift << endl;
-//     cv_classify_NN CV_onesegment(path, path_dataset, actionNames, scale_factor, shift,  dim);
-//     vec_logEucl(i) = CV_onesegment.logEucl();
-//    
-//   }
-//   
-//   vec_logEucl.t().print();
+   vec vec_shift;
+   vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
+   int scale_factor = 1;
+   int dim = 14; 
+   int best_p = 2;
+ 
+   
+   vec vec_logEucl   	= zeros(vec_shift.n_elem);
+   vec vec_proj_grass	= zeros(vec_shift.n_elem);
+   
+   for (int i=0; i< vec_shift.n_elem; ++i)
+   {
+     
+     int shift = vec_shift(i);
+     cout << "Shift: " << shift << endl;
+     cv_classify_NN CV_onesegment(path, path_dataset, actionNames, scale_factor, shift,  dim);
+     vec_logEucl(i) = CV_onesegment.logEucl();
+     vec_proj_grass(i) = CV_onesegment.proj_grass(best_p);
+    
+   }
+   
+   vec_logEucl.t().print("vec_logEucl");
+   vec_proj_grass.print("vec_proj_grass");
   
   
   
   
   ///Scales
-   int shift = 0;
-   vec scale_vec;
-   scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95 << 1 << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;
-   int dim = 14; 
-  
-  vec vec_logEucl   = zeros(scale_vec.n_elem);
-  
-  for (int i=0; i< scale_vec.n_elem; ++i)
-  {
-     float scale_factor = scale_vec(i);
-     cout << "scale_factor: " << scale_factor << endl;
-     cv_classify_NN CV_onesegment(path, path_dataset, actionNames, scale_factor, shift,  dim);
-     vec_logEucl(i) = CV_onesegment.logEucl();
-  }
-
-  
-  vec_logEucl.t().print();
+//    int shift = 0;
+//    vec scale_vec;
+//    scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95 << 1 << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;
+//    int dim = 14; 
+//   
+//   vec vec_logEucl   = zeros(scale_vec.n_elem);
+//   
+//   for (int i=0; i< scale_vec.n_elem; ++i)
+//   {
+//      float scale_factor = scale_vec(i);
+//      cout << "scale_factor: " << scale_factor << endl;
+//      cv_classify_NN CV_onesegment(path, path_dataset, actionNames, scale_factor, shift,  dim);
+//      vec_logEucl(i) = CV_onesegment.logEucl();
+//   }
+// 
+//   
+//   vec_logEucl.t().print();
   
   
   
