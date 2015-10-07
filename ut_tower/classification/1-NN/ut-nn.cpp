1c00ff6df649f471dc8cb8cdcb93d3864e40702d
johanna-codes <johacarvajalg@gmail.com>
2015-10-07 11:30:02 +1000

    PM for UT all scales

--- ut_tower/classification/1-NN/ut-nn.cpp
+++ ut_tower/classification/1-NN/ut-nn.cpp
@@ -51,84 +51,109 @@ main(int argc, char** argv)
   //CV_onesegment.logEucl();
   
   ///LED: Cross Validation. All scales
-//    int shift = 0;
-//    vec scale_vec;
-//    scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95 << 1 << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;
-//    int dim = 14; 
-//   vec vec_logEucl   = zeros(scale_vec.n_elem);
-//   
-//   for (int i=0; i< scale_vec.n_elem; ++i)
-//   {
-//     
-//     float scale_factor = scale_vec(i);
-//     cout << "scale_factor: " << scale_factor << endl;
-//     cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
-//     vec_logEucl(i) = CV_onesegment.logEucl();
-//     
-//   }
-//   
-//   cout << "Performance for each shift" << endl;
-//   vec_logEucl.t().print();
-
-  
-//   ///LED: Cross Validation. All shifts
-//   vec vec_shift;
-//   vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
-//   int scale_factor = 1;
-//   int dim = 14; 
-//   vec vec_logEucl   = zeros(vec_shift.n_elem);
-//   
-//   for (int i=0; i< vec_shift.n_elem; ++i)
-//   {
-//     
-//     int shift = vec_shift(i);
-//     cout << "Shift: " << shift << endl;
-//     cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
-//     vec_logEucl(i) = CV_onesegment.logEucl();
-//     
-//   }
-//   
-//   cout << "Performance for each shift" << endl;
-//   vec_logEucl.t().print();
-  
-  
-  
-  ///PM: Cross Validation. One shift
-  //    vec vec_pm = zeros(dim);
-  //    for (int p=1; p<= dim; ++p)
-  //    {
-  //      cout << "p= " << p << endl;
-  //      cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
-  //      vec_pm(p-1) = CV_onesegment.proj_grass(p);
-  //    }
-  //    
-  //    vec_pm.t().print("Projection Metric");
+  //    int shift = 0;
+  //    vec scale_vec;
+  //    scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95 << 1 << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;
+  //    int dim = 14; 
+  //   vec vec_logEucl   = zeros(scale_vec.n_elem);
   //   
-  
-   ///PM: Cross Validation. All shifts
-   vec vec_shift;
-   vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
-   int scale_factor = 1;
-   int dim = 14; 
-   vec vec_proj_grass   = zeros(vec_shift.n_elem);
-   int best_p = 4;
-   
-   for (int i=0; i< vec_shift.n_elem; ++i)
-   {
-     
-     int shift = vec_shift(i);
-     cout << "Shift: " << shift << endl;
-     cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
-     vec_proj_grass(i) = CV_onesegment.proj_grass(best_p);
-     
-   }
-   
-   cout << "Performance for each shift" << endl;
-   vec_proj_grass.t().print();
-  
-  
-  
-  
-  return 0;
-  
-}
+  //   for (int i=0; i< scale_vec.n_elem; ++i)
+  //   {
+    //     
+    //     float scale_factor = scale_vec(i);
+    //     cout << "scale_factor: " << scale_factor << endl;
+    //     cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
+    //     vec_logEucl(i) = CV_onesegment.logEucl();
+    //     
+    //   }
+    //   
+    //   cout << "Performance for each shift" << endl;
+    //   vec_logEucl.t().print();
+    
+    
+    //   ///LED: Cross Validation. All shifts
+    //   vec vec_shift;
+    //   vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
+    //   int scale_factor = 1;
+    //   int dim = 14; 
+    //   vec vec_logEucl   = zeros(vec_shift.n_elem);
+    //   
+    //   for (int i=0; i< vec_shift.n_elem; ++i)
+    //   {
+      //     
+      //     int shift = vec_shift(i);
+      //     cout << "Shift: " << shift << endl;
+      //     cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
+      //     vec_logEucl(i) = CV_onesegment.logEucl();
+      //     
+      //   }
+      //   
+      //   cout << "Performance for each shift" << endl;
+      //   vec_logEucl.t().print();
+      
+      
+      
+      ///PM: Cross Validation. One shift
+      //    vec vec_pm = zeros(dim);
+      //    for (int p=1; p<= dim; ++p)
+      //    {
+	//      cout << "p= " << p << endl;
+	//      cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
+	//      vec_pm(p-1) = CV_onesegment.proj_grass(p);
+	//    }
+	//    
+	//    vec_pm.t().print("Projection Metric");
+	//   
+	
+	///PM: Cross Validation. All shifts
+	//    vec vec_shift;
+	//    vec_shift << -25 << -20 << -15 << -10 << -5 << 0 << 5 << 10 << 15 << 20 << 25 << endr;
+	//    int scale_factor = 1;
+	//    int dim = 14; 
+	//    vec vec_proj_grass   = zeros(vec_shift.n_elem);
+	//    int best_p = 4;
+	//    
+	//    for (int i=0; i< vec_shift.n_elem; ++i)
+	//    {
+	  //      
+	  //      int shift = vec_shift(i);
+	  //      cout << "Shift: " << shift << endl;
+	  //      cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
+	  //      vec_proj_grass(i) = CV_onesegment.proj_grass(best_p);
+	  //      
+	  //    }
+	  //    
+	  //    cout << "Performance for each shift" << endl;
+	  //    vec_proj_grass.t().print();
+	  
+	  
+	  ///PM: Cross Validation. All scales
+	  
+	  int best_p = 4;	  
+	  int shift = 0;
+	  vec scale_vec;
+	  scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95 << 1 << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;
+	  int dim = 14; 
+	  vec vec_proj_grass   = zeros(scale_vec.n_elem);
+	  
+	  for (int i=0; i< scale_vec.n_elem; ++i)
+	  {
+	    
+	    float scale_factor = scale_vec(i);
+	    cout << "scale_factor: " << scale_factor << endl;
+	    cv_classify_NN CV_onesegment(path, actionNames, scale_factor, shift,  dim);
+	    vec_proj_grass(i) = CV_onesegment.proj_grass(best_p);
+	    
+	  }
+	  
+	  cout << "Performance for each shift" << endl;
+	  vec_proj_grass.t().print(); 
+	  
+	  
+	  
+	  
+	  
+	  return 0;
+	  
+      }
+      
