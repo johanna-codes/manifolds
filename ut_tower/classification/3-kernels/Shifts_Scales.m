945ee3ab6204e890f2718695c04d645dd3bb18a3
johanna-codes <johacarvajalg@gmail.com>
2015-10-07 13:34:08 +1000

    Run Shifts Scales. Kernel proj-rbf. UT

--- ut_tower/classification/3-kernels/Shifts_Scales.m
+++ ut_tower/classification/3-kernels/Shifts_Scales.m
@@ -13,7 +13,11 @@ action_seq_names = list_video_sequences;
 dim =14;
 num_videos = 108; %From the dataset description (108).
 
-%% Kernel LED-Poly. UT Tower. Shifts
+%% Kernel LED-Poly. 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%UT Tower. Shifts
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % display('Testing svm + Kernel LED-POLY');
 % 
@@ -29,8 +33,9 @@ num_videos = 108; %From the dataset description (108).
 %     test_acc_shifts(i) = acc;
 % end
 
-
-%% Kernel LED-Poly. UT Tower. Scales
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%UT Tower. Scales
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 display('Testing svm + Kernel LED-POLY');
 
@@ -46,3 +51,42 @@ for i=1:length(vec_scale)
     vec_scale_scales(i) = acc;
 end
 
+%% Kernel proj-rbf. 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%UT Tower. Shifts
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+display('Testing svm + Kernel Proj-RBF');
+
+scale  = 1;
+best_p = 4;
+best_delta = 10;
+vec_shift = [ -25, -20, -15, -10, -5,  0, 5, 10, 15, 20, 25 ];
+test_acc_shifts = zeros(length(vec_shift),1);
+
+for i=1:length(vec_shift)
+    shift = vec_shift(i);
+    sprintf('shift = %d ', shift)
+    acc = ProjectionRBF_test(path, action_seq_names, scale, shift, best_delta, dim, best_p, num_videos);
+    test_acc_shifts(i) = acc;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%UT Tower. Scales
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+display('Testing svm + Kernel Proj-RBF');
+
+shift  = 0;
+best_p = 4;
+best_delta = 10;
+vec_scale = [0.75 0.80 0.85 0.90 0.95 1 1.05 1.10  1.15 1.20 1.25];
+test_acc = zeros(length(vec_scale),1);
+
+for i=1:length(vec_scale)
+    scale = vec_scale(i);
+    sprintf('scale = %d ', scale)
+    acc = ProjectionRBF_test(path, action_seq_names, scale, shift, best_delta, best_p, in_p, num_videos);
+    vec_scale_scales(i) = acc;
+end
