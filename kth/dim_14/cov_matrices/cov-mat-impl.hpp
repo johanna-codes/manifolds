73c2c6c149786df592f28fa21b68c0738f18d050
johanna-codes <johacarvajalg@gmail.com>
2015-10-08 17:10:44 +1000

    Run all COVS for shifts Hor & Ver. KTH

--- kth/dim_14/cov_matrices/cov-mat-impl.hpp
+++ kth/dim_14/cov_matrices/cov-mat-impl.hpp
@@ -287,11 +287,11 @@ inline
 void
 cov_mat_kth::one_video_one_cov( std::string load_feat_video_i, std::string load_labels_video_i, int sc, int pe, int act )
 {
-  #pragma omp critical
-  {
-  cout << load_feat_video_i << endl;
-  getchar();
-  }
+//   #pragma omp critical
+//   {
+//   cout << load_feat_video_i << endl;
+//   getchar();
+//   }
   
   mat mat_features_video_i;
   
