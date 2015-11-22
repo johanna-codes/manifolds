0746427eb1ed041767304c2d41728e00fedca6ed
johanna-codes <johacarvajalg@gmail.com>
2015-11-23 08:41:10 +1000

    Updating RSR

--- kth/dim_14/RSR/kth_RSR.m
+++ kth/dim_14/RSR/kth_RSR.m
@@ -5,6 +5,7 @@ clc
 % Sparse Coding and Dictionary Learning for Symmetric
 %Positive Definite Matrices: A Kernel Approach
 %ECCV 2012
+dbstop error;
 
 addpath('/home/johanna/codes/Mehrtash/harandi_eccv_2012_matlab/');% --> Wanda
 
@@ -47,6 +48,8 @@ for b=1:length(vec_Beta)
         X_train = zeros(dim,dim,n_test);
         labels_train = zeros(1,n_test);
         k =1;
+        
+        %Joining Testing Data
         for pe_tr=1: n_peo
             if pe_tr~=pe_ts
                 for act=1: n_actions
@@ -75,63 +78,17 @@ for b=1:length(vec_Beta)
             j = j+1;
         end
         
+        
+        %Creating Sets
         TrainSet(1).X = X_train;
         TrainSet(1).y = labels_train;
         
         TestSet(1).X = X_test;
         TestSet(1).y = labels_test;
         
-        %% As per example in Code
-        
-        SPARSE_SOLVER = 'CVX'; %options are CVX and SPAMS
-        
-        %Generate Kernel
-        galleryKernel = exp(-Beta*Stein_Divergence(TrainSet.X,TrainSet.X));
-        probeKernel = exp(-Beta*Stein_Divergence(TestSet.X,TrainSet.X))';
-        
-        
-        %Normalize dictionary
-        KD = galleryKernel./repmat(sqrt(sum(galleryKernel.^2)),[size(galleryKernel,1) 1]);
-        
-        %
-        KX = probeKernel./repmat(sqrt(sum(probeKernel.^2)),[size(probeKernel,1) 1]);
-        L1 = size(probeKernel,2);
-        if (strcmpi(SPARSE_SOLVER,'CVX'))
-            SR_Lambda = SR_Lambda_input;
-            A = KD;
-            Number_Visual_Codes = size(A,2);
-            scX = zeros(Number_Visual_Codes,L1);
-            for tmpC1=1:L1
-                if (mod(tmpC1,10) == 1)
-                    %fprintf('Working on sample %d/%d\n',tmpC1,L1);
-                end
-                cvx_begin quiet
-                variable tmp_Sparse_X(Number_Visual_Codes);
-                minimize( -2*tmp_Sparse_X'*KX(:,tmpC1)+tmp_Sparse_X'*KD*tmp_Sparse_X+SR_Lambda*norm(tmp_Sparse_X,1));
-                % if you are interested in positive sparse codes, uncomment the following two lines
-                %             subject to
-                %                 tmp_Sparse_X >= 0;
-                cvx_end
-                scX(:,tmpC1) = tmp_Sparse_X;
-            end %endfor
-        elseif (strcmpi(SPARSE_SOLVER,'SPAMS'))
-            SR_Lambda = 1e-2;
-            [KD_U,KD_D,~] = svd(KD);
-            A = diag(sqrt(diag(KD_D)))*KD_U';
-            D_Inv = KD_U*diag(1./sqrt(diag(KD_D)));
-            KX = D_Inv'*KX;
-            scX = full(mexLasso(KX,A,struct('mode',2,'lambda',SR_Lambda,'lambda2',0)));
-        else
-            error('Sparse solver is not defined. This version accepts CVX or SPAMS');
-        end
-        
-        
-        %% Classification
-        qLabel = Classify_SRC(A,TrainSet.y,scX,KX);
-        
-        
-        
-        CRR = sum((qLabel - TestSet.y == 0))/L1;
+        %% As per example in Code. RSR
+        CRR = RSR(TrainSet,TestSet,Beta,SR_Lambda_input);
+
         acc = [acc CRR];
         
         %disp('Press a Key');
