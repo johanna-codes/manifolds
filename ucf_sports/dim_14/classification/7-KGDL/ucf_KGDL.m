42916cdaf68ed08890f4844c2dfbd04520c5dcfd
johanna-codes <johacarvajalg@gmail.com>
2015-11-23 11:36:15 +1000

    Adding KGDL for UCF

--- ucf_sports/dim_14/classification/7-KGDL/ucf_KGDL.m
+++ ucf_sports/dim_14/classification/7-KGDL/ucf_KGDL.m
@@ -26,83 +26,88 @@ scale_factor = 1;
 shift = 0;
 
 
-%prompt = 'SR_Lambda? ';
-%SR_Lambda_input = input(prompt);
-SR_Lambda_input = 1e-1;
+Solver_Flag = 1;  %1: SPAMS, 2: CVX
 
-%prompt = 'Beta ';
-%Beta = input(prompt);
-vec_Beta = 1:1:10;
-all_means = zeros(length(vec_Beta),1);
+SR_lambda = 1e-1;    %sparse representation parameter
+nAtoms = 128;        %size of the dictionary
+dict_options.L = 20; %number of non-zero elements in OMP for dictionary learning
+
+
+
 acc = [];
 
 
-for b=1:length(vec_Beta)
-    Beta = vec_Beta(b)
-    load_sub_path =strcat(path, 'dim_', int2str(dim), '/cov_matrices/one-cov-mat/scale', int2str(scale_factor), '-shift', int2str(shift) );
+
+load_sub_path =strcat(path, 'dim_', int2str(dim), '/cov_matrices/one-cov-mat/scale', int2str(scale_factor), '-shift', int2str(shift) );
+
+
+for video_ts= 1: n_videos %One Run
+    X_train = zeros(dim,dim,n_test);
+    labels_train = zeros(n_test,1);
+    k =1;
     
+    action_name = action_seq_names(video_ts,1);
+    folder_n    = action_seq_names(video_ts,2);
     
-    for video_ts= 1: n_videos %One Run
-        X_train = zeros(dim,dim,n_test);
-        labels_train = zeros(n_test,1);
-        k =1;
-        
-        action_name = action_seq_names(video_ts,1);
-        folder_n    = action_seq_names(video_ts,2);
-        
-        if (~(strcmp(action_name,'Run-Side') && strcmp(folder_n,'001')))
-            %ts = [action_name,'_',folder_n];
-            %disp(ts);
-            for video_tr=1: n_videos
-                if (video_tr~=video_ts)
-                    action_name_tr = action_seq_names(video_tr,1);
-                    folder_n_tr    = action_seq_names(video_tr,2);
-                    act_tr  =  str2double( action_seq_names(video_tr,3) );
-                    if (~(strcmp(action_name_tr, 'Run-Side') && strcmp(folder_n_tr,'001')))
-                        %tr = [action_name_tr,'_',folder_n_tr];
-                        %disp(tr);
-                        name_load_cov = strcat( load_sub_path, '/cov_', action_name_tr, '_', folder_n_tr, '_dim', int2str(dim), '.h5');
-                        hinfo = hdf5info( char(name_load_cov) );
-                        one_video = hdf5read(hinfo.GroupHierarchy.Datasets(1));
-                        %disp(one_video);
-                        X_train(:,:,k) = one_video;
-                        labels_train(k) = act_tr;
-                        k=k+1;
-                    end
+    if (~(strcmp(action_name,'Run-Side') && strcmp(folder_n,'001')))
+        %ts = [action_name,'_',folder_n];
+        %disp(ts);
+        for video_tr=1: n_videos
+            if (video_tr~=video_ts)
+                action_name_tr = action_seq_names(video_tr,1);
+                folder_n_tr    = action_seq_names(video_tr,2);
+                act_tr  =  str2double( action_seq_names(video_tr,3) );
+                if (~(strcmp(action_name_tr, 'Run-Side') && strcmp(folder_n_tr,'001')))
+                    %tr = [action_name_tr,'_',folder_n_tr];
+                    %disp(tr);
+                    name_load_cov = strcat( load_sub_path, '/cov_', action_name_tr, '_', folder_n_tr, '_dim', int2str(dim), '.h5');
+                    hinfo = hdf5info( char(name_load_cov) );
+                    one_video = hdf5read(hinfo.GroupHierarchy.Datasets(1));
+                    %disp(one_video);
+                    X_train(:,:,k) = one_video;
+                    labels_train(k) = act_tr;
+                    k=k+1;
                 end
             end
         end
-        %Testing Set only has one video
-        if (~(strcmp(action_name,'Run-Side') && strcmp(folder_n,'001')))
-            action_name = action_seq_names(video_ts,1);
-            folder_ns    = action_seq_names(video_ts,2);
-            act_ts  =  str2double( action_seq_names(video_ts,3) );
-            
-            labels_test = act_ts;
-            disp('Testing with: ');
-            name_load_cov = strcat( load_sub_path, '/cov_', action_name, '_', folder_n, '_dim', int2str(dim), '.h5')
-            hinfo = hdf5info( char(name_load_cov) );
-            one_video = hdf5read(hinfo.GroupHierarchy.Datasets(1));
-            X_test(:,:,1) = one_video;
-            
-            
-            
-            
-            TrainSet(1).X = X_train;
-            TrainSet(1).y = labels_train;
-            
-            TestSet(1).X = X_test;
-            TestSet(1).y = labels_test;
-            
-            %% As per example in Code
-            
-            CRR = RSR(TrainSet,TestSet,Beta,SR_Lambda_input);
-            fprintf('Correct recognition accuracy with a labeled dictionary : %.1f%%.\n',100*CRR);
-            
-            acc = [acc CRR];
-        end
     end
-    
-    all_means(b) = CRR*100;
+    %Testing Set only has one video
+    if (~(strcmp(action_name,'Run-Side') && strcmp(folder_n,'001')))
+        action_name = action_seq_names(video_ts,1);
+        folder_ns    = action_seq_names(video_ts,2);
+        act_ts  =  str2double( action_seq_names(video_ts,3) );
+        
+        labels_test = act_ts;
+        disp('Testing with: ');
+        name_load_cov = strcat( load_sub_path, '/cov_', action_name, '_', folder_n, '_dim', int2str(dim), '.h5')
+        hinfo = hdf5info( char(name_load_cov) );
+        one_video = hdf5read(hinfo.GroupHierarchy.Datasets(1));
+        X_test(:,:,1) = one_video;
+        
+        
+        
+        
+        TrainSet(1).X = X_train;
+        TrainSet(1).y = labels_train;
+        
+        TestSet(1).X = X_test;
+        TestSet(1).y = labels_test;
+        
+        %% As per example in Code
+        
+        
+        CRR = KGDL(trn, tst, Solver_Flag, SR_lambda,nAtoms,dict_options);
+        fprintf('Correct recognition accuracy with a labeled dictionary : %.1f%%.\n',100*CRR);
+        acc = [acc CRR];
+        
+        
+        
+        
+        %acc = [acc CRR];
+    end
 end
+
+fprintf('Cross Validation Accuracy: %.1f%%.\n',mean(acc)*100);
+
+
 
