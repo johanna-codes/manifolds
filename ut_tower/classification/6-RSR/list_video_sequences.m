a3fd4080d5226c53502bd889e1c423c644fda7c6
johanna-codes <johacarvajalg@gmail.com>
2015-11-23 16:19:26 +1000

    Adding RSR for UT_Tower

---
+++ ut_tower/classification/6-RSR/list_video_sequences.m
@@ -0,0 +1,24 @@
+function action_seq_names = list_video_sequences(path_dataset)
+
+
+actions = importdata('actionNames.txt');
+n_actions = size(actions,1);
+k = 1;
+action_seq_names=cell(108,3);
+%load_sub_path =strcat(path, 'cov_matrices/kth-one-cov-mat-dim', int2str(dim), '/sc', int2str(sc), '/scale', int2str(scale_factor), '-shift', int2str(shift) );
+
+for act=1:n_actions
+   
+  
+ for seq=1:12
+   action_seq_names(k,1) = actions(act); %Action
+   action_seq_names(k,2) = {int2str(act)}; %Action ID
+   action_seq_names(k,3) = {int2str(seq-1)}; %Video Sequence. It starts at 0
+   k=k+1;
+     
+ end
+ 
+ 
+
+end
+
