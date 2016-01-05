158a81163d15f0ecda90b03774a2ba1271438a0a
johanna-codes <johacarvajalg@gmail.com>
2016-01-05 15:47:42 +1000

    Experiments with KTH & FV. Only Covs

--- kth/dim_14/FV/FV_kth_all_videos.m
+++ kth/dim_14/FV/FV_kth_all_videos.m
@@ -57,7 +57,7 @@ for i=1:n_people
         end
         %to save
         
-        save_name = strcat('./FV_training/scale',num2str(scale_factor), '-shift',  int2str(shift),  '/FV_', people(i),'_',actionNames(j),'_sc', sc, '_Ng', Ng, '.txt');
+        save_name = strcat('./FV_training/scale',num2str(scale_factor), '-shift',  int2str(shift),  '/FV_', people(i),'_',actionNames(j),'_sc', sc, '_Ng', Ng, '_NOmeans', '.txt');
         sSave = char(save_name);
         %display(sSave);
         fid1=fopen(sSave,'wt');
