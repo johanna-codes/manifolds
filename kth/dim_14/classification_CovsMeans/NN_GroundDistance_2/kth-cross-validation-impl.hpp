c03e38add36d048da1c3752bf1640c0a39db709b
johanna-codes <johacarvajalg@gmail.com>
2016-01-13 11:57:32 +1000

    Adding NN using Ground Distance (GD based on product of Lie Groups)

--- kth/dim_14/classification_CovsMeans/NN_GroundDistance_2/kth-cross-validation-impl.hpp
+++ kth/dim_14/classification_CovsMeans/NN_GroundDistance_2/kth-cross-validation-impl.hpp
@@ -170,8 +170,13 @@ kth_cv_omp::GD_one_video(int pe_test, std::string load_sub_path, std::string loa
 	   train_logM_Cov.load( load_logM_Covs_tr.str() );
 	   train_Mean.load( load_Means_tr.str() );
 	   
+	   double subs, sum;
 	   
-	   tmp_dist_a =sqrt( as_scalar( ( test_Mean - train_Mean ).t()*( inv( test_Cov ) + inv( train_Cov ) )*( test_Mean - train_Mean ) ) );
+	   subs = ( test_Mean - train_Mean );
+	   sum = inv( test_Cov ) + inv( train_Cov );
+	   cout << "tmp_dist_a" << endl;
+	   
+	   tmp_dist_a =sqrt( as_scalar( subs.t()*sum*subs ) );
 	   tmp_dist_b = norm( test_logM_Cov - train_logM_Cov, "fro");
     
 	   dist = (1-theta)*tmp_dist_a + theta*tmp_dist_b;
