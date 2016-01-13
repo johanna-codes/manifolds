f535f8cbc0785ffeb9aa1915a9ef284dbc99318d
johanna-codes <johacarvajalg@gmail.com>
2016-01-13 17:08:14 +1000

    NN with several  GMM. KTH. Ground Distance 2

--- kth/dim_14/classification_CovsMeans/NN_GroundDistance_2/kth-cross-validation-impl.hpp
+++ kth/dim_14/classification_CovsMeans/NN_GroundDistance_2/kth-cross-validation-impl.hpp
@@ -89,7 +89,7 @@ kth_cv_omp::Ground_Distance_GMM(const int Ng)
   }
   
   est_labels.t().print();
-  est_labels.t().print();
+  real_labels.t().print();
   //   real_labels.save("./results_onesegment/Log_Eucl_real_labels.dat", raw_ascii);
   //   est_labels.save("./results_onesegment/Log_Eucl_est_labels.dat", raw_ascii);
   //   test_video_list.save("./results_onesegment/Log_Eucl_test_video_list.dat", raw_ascii);
