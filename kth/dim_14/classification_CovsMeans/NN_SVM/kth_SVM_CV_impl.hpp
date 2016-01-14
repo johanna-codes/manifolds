c7711810ae174d344b8d67350c3ad66889b5682a
johanna-codes <johacarvajalg@gmail.com>
2016-01-14 16:22:30 +1000

    Adding NN + GD_2 + SVM. One Cov & One Gaussian

--- kth/dim_14/classification_CovsMeans/NN_SVM/kth_SVM_CV_impl.hpp
+++ kth/dim_14/classification_CovsMeans/NN_SVM/kth_SVM_CV_impl.hpp
@@ -411,6 +411,7 @@ vec
 kth_cv_distNN_svm::get_emb_LogCov(mat cov_i, vec mean_i)
 {
  
+  double THRESH = 0.000001;
   mat CovMean = zeros(dim+1,dim+1);
   mat cov_i_2 = cov_i + mean_i*mean_i.t();
   CovMean.submat( 0, 0, dim-1, dim-1) = cov_i_2;    
