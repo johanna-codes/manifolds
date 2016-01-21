9c827f3df87909da7dc648a11deed3ea477237fb
johanna-codes <johacarvajalg@gmail.com>
2016-01-21 13:35:58 +1000

    Get features. Varying the scale

--- kth/dim_14/covs_means_matrices_vectors/kth-Saving_CovMeans-mat.cpp
+++ kth/dim_14/covs_means_matrices_vectors/kth-Saving_CovMeans-mat.cpp
@@ -49,27 +49,48 @@ main(int argc, char** argv)
    all_people.load(peopleList);
    
   
-   float scale_factor = 1;
-   int shift = 0;
+  
 
    
    //cout << arma_version::as_string() << endl;
    
    
-   for (int i = 0; i< vec_Ng.n_elem; ++i)
+//    // Ideal (Clean) Data
+//   float scale_factor = 1;
+//   int shift = 0;
+//    for (int i = 0; i< vec_Ng.n_elem; ++i)
+//    {
+//      int Ng = vec_Ng(i);
+//      cout << Ng << endl;
+//      
+//      CovMeans_mat_kth get_CovMeans(path, actionNames, scale_factor, shift, total_scenes);
+//      //get_CovMeans.calculate_one_per_video( all_people, dim );
+//      get_CovMeans.get_gmm_per_video( all_people, dim, Ng );
+//    
+//    }
+//   
+   
+   
+   //  
+   
+   ///Varying the Scale
+   
+   int shift = 0;
+   vec scale_vec;
+   scale_vec << 0.75 <<  0.80 << 0.85 << 0.90 << 0.95  << 1.05 << 1.10 << 1.15 << 1.20 << 1.25 << endr;
+   
+   for (int i = 0; i< scale_vec.n_elem; ++i)
    {
-     int Ng = vec_Ng(i);
-     cout << Ng << endl;
-     
+     int scale_factor = scale_vec(i);
+     cout << "scale_factor= " << scale_factor << endl;
      
      CovMeans_mat_kth get_CovMeans(path, actionNames, scale_factor, shift, total_scenes);
-     //get_CovMeans.calculate_one_per_video( all_people, dim );
-     get_CovMeans.get_gmm_per_video( all_people, dim, Ng );
+     get_CovMeans.calculate_one_per_video( all_people, dim );
    
    }
    
- 
-  
+   
+   
   
   
 ///Shifting Videos
