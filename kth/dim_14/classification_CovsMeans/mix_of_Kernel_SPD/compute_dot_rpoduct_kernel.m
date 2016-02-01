482d407b8970d0c610047531250b1b59df1a7671
johanna-codes <johacarvajalg@gmail.com>
2016-02-01 11:07:28 +1000

    Adding mix of Kernels

--- kth/dim_14/classification_CovsMeans/mix_of_Kernel_SPD/compute_dot_rpoduct_kernel.m
+++ kth/dim_14/classification_CovsMeans/mix_of_Kernel_SPD/compute_dot_rpoduct_kernel.m
@@ -7,7 +7,7 @@ size(X_2)
 
 for x_1 = 1: size(X_1,3)
     for x_2 = 1:size(X_2,3)
-          K(x_1,x_2) = X_1(:,:,x_1), X_2(:,:,x_2);
+          K(x_1,x_2) = X_1(:,:,x_1)'*X_2(:,:,x_2);
           K(x_1,x_2)
           pause
     end
