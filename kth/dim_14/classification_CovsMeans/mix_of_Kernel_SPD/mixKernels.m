3aab5ce2125eea351c90df513e4af61bbb6fd954
johanna-codes <johacarvajalg@gmail.com>
2016-02-01 12:00:51 +1000

    Adding mix of Kernels

--- kth/dim_14/classification_CovsMeans/mix_of_Kernel_SPD/mixKernels.m
+++ kth/dim_14/classification_CovsMeans/mix_of_Kernel_SPD/mixKernels.m
@@ -33,7 +33,7 @@ vec_alpha = 0.1:0.1:1;
  
   for i=1:length(vec_alpha)
          alpha = vec_alpha(i);
-         X=sprintf('n = %5.2f, ', lpha);
+         X=sprintf('n = %5.2f, ', alpha);
          disp(X);
          acc = kth_test(path,scale, shift, dim, best_n, alpha);
          test_acc(i) = acc;
