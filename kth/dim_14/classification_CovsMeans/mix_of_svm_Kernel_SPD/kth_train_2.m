c80c31a1882677a1592f24f4f818684c2e0933f4
johanna-codes <johacarvajalg@gmail.com>
2016-02-04 12:20:13 +1000

    Adding mix of Kernels + linear SVM

--- kth/dim_14/classification_CovsMeans/mix_of_svm_Kernel_SPD/kth_train_2.m
+++ kth/dim_14/classification_CovsMeans/mix_of_svm_Kernel_SPD/kth_train_2.m
@@ -26,7 +26,6 @@ for pe_ts= 1: n_peo
     disp(X);
     
     new_features = zeros(n_test,30);    %30 = 15*2.  15 es la salida del svm 
-
     
     load_svm_model =strcat( './svm_models_output1/svm1_2_run',num2str(pe_ts), '.mat');
     load(load_svm_model); %loading  'model_1', 'model_2', 'X_train_covs', 'X_train_means'
