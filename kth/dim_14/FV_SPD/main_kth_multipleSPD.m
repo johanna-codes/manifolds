db8f3c29c010cc85b1c0d95a1bc2c2e744edc0f2
johanna-codes <johacarvajalg@gmail.com>
2016-02-09 12:22:46 +1000

    Thinking. Use GMM

--- kth/dim_14/FV_SPD/main_kth_multipleSPD.m
+++ kth/dim_14/FV_SPD/main_kth_multipleSPD.m
@@ -23,7 +23,7 @@ vec_numSPD = [ 2 4 8 16 32 60 128 256];
 
 dim_spdvec  = dim*( dim + 1 )/2;
 
-Ng = int2str(Ncent);
+
 
 actions = importdata('actionNames.txt');
 all_people = importdata('people_list.txt');
@@ -55,33 +55,44 @@ end
 scale_factor = 1;
 shift = 0;
 load_sub_path =strcat('./FV_training_mulSPD/scale', int2str(scale_factor), '-shift',  int2str(shift));
-for pe_ts= 1: n_peo
-    X_train = zeros(dim_FV,n_test);
-    labels_train = zeros(n_test,1);
-    k =1;
-    for pe_tr=1: n_peo
-        if pe_tr~=pe_ts
-            for act=1: n_actions
-                %show_you = strcat(all_people(pe_tr),  '_', actions(act));
-                %disp(show_you);
-                name_load_FV = strcat( load_sub_path, '/FV_', all_people(pe_tr),'_',actions(act),'_sc', sc, '_Ng', Ng,  '_numSPD_', num2str(numSPD), '.txt');
-                one_FV = load( char( name_load_FV ) );
-                X_train(:,k) = one_FV;
-                labels_train(k) = act;
-                k=k+1;
+
+for i=1:length(vec_Ncent)
+    Ncent = vec_Ncent(i);
+    for j=1:length(vec_numSPD)
+        numSPD = vec_numSPD(j);
+        Ng = int2str(Ncent);
+        
+        
+        for pe_ts= 1: n_peo
+            X_train = zeros(dim_FV,n_test);
+            labels_train = zeros(n_test,1);
+            k =1;
+            for pe_tr=1: n_peo
+                if pe_tr~=pe_ts
+                    for act=1: n_actions
+                        %show_you = strcat(all_people(pe_tr),  '_', actions(act));
+                        %disp(show_you);
+                        
+                        name_load_FV = strcat( load_sub_path, '/FV_', all_people(pe_tr),'_',actions(act),'_sc', sc, '_Ng', Ng,  '_numSPD_', num2str(numSPD), '.txt');
+                        one_FV = load( char( name_load_FV ) );
+                        X_train(:,k) = one_FV;
+                        labels_train(k) = act;
+                        k=k+1;
+                    end
+                end
             end
+            
+            %size(X_train);
+            data_train = X_train';
+            model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1 -q' ]);
+            save_svm_model =strcat( './svm_models/run_person', int2str(pe_ts),  '_Ng', Ng,  '_numSPD_', num2str(numSPD), '.mat');
+            save(save_svm_model, 'model');
+            
         end
     end
-    
-    %size(X_train);
-    data_train = X_train';
-    model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1 -q' ]);
-    save_svm_model =strcat( './svm_models/run_person', int2str(pe_ts), '.mat');
-    save(save_svm_model, 'model');
-    
 end
 
-%% Testing 
+%% Testing
 
 scale_factor = 1;
 shift = 0;
@@ -101,7 +112,7 @@ for i=1:length(vec_Ncent)
         j=1;
         for pe_ts= 1: n_peo
             
-            load_svm_model = strcat( './svm_models/run_person', int2str(pe_ts), '.mat');
+            load_svm_model = strcat( './svm_models/run_person', int2str(pe_ts),  '_Ng', Ng,  '_numSPD_', num2str(numSPD), '.mat');
             load(load_svm_model); %loading model and X_train
             
             for act_ts = 1:n_actions
