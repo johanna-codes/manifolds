8de55a4375786cea00bace628c95b662bfb8352d
johanna-codes <johacarvajalg@gmail.com>
2016-02-09 16:11:29 +1000

    Thinking. Use GMM

--- kth/dim_14/FV_SPD/main_kth_multipleSPD.m
+++ kth/dim_14/FV_SPD/main_kth_multipleSPD.m
@@ -38,62 +38,62 @@ n_test = (n_peo-1)*n_actions;
 
 
 %% Get FV. Run Just once
- scale_factor = 1;
-shift =0;
-
-for i=5:length(vec_Ncent)
-    Ncent = vec_Ncent(i);
-    for j=1:length(vec_numSPD)
-        numSPD = vec_numSPD(j);
-        fprintf('Ng: %d - numSPD %d \n',Ncent, numSPD);
-
-        FV_kth_all_videos_mltipleSPD(Ncent, numSPD, dim_spdvec, scale_factor, shift);
-    end
-end
+%  scale_factor = 1;
+% shift =0;
+% 
+% for i=5:length(vec_Ncent)
+%     Ncent = vec_Ncent(i);
+%     for j=1:length(vec_numSPD)
+%         numSPD = vec_numSPD(j);
+%         fprintf('Ng: %d - numSPD %d \n',Ncent, numSPD);
+% 
+%         FV_kth_all_videos_mltipleSPD(Ncent, numSPD, dim_spdvec, scale_factor, shift);
+%     end
+% end
 
 
 
 %% Training
-scale_factor = 1;
-shift = 0;
-load_sub_path =strcat('./FV_training_mulSPD/scale', int2str(scale_factor), '-shift',  int2str(shift));
-
-for i=1:length(vec_Ncent)
-    Ncent = vec_Ncent(i);
-    for j=1:length(vec_numSPD)
-        numSPD = vec_numSPD(j);
-        Ng = int2str(Ncent);
-        dim_FV = 2*dim_spdvec*Ncent;
-        fprintf('Training with Ng: %d - numSPD %d \n',Ncent, numSPD);
-
-        for pe_ts= 1: n_peo
-            X_train = zeros(dim_FV,n_test);
-            labels_train = zeros(n_test,1);
-            k =1;
-            for pe_tr=1: n_peo
-                if pe_tr~=pe_ts
-                    for act=1: n_actions
-                        %show_you = strcat(all_people(pe_tr),  '_', actions(act));
-                        %disp(show_you);
-                        
-                        name_load_FV = strcat( load_sub_path, '/FV_', all_people(pe_tr),'_',actions(act),'_sc', sc, '_Ng', Ng,  '_numSPD_', num2str(numSPD), '.txt');
-                        one_FV = load( char( name_load_FV ) );
-                        X_train(:,k) = one_FV;
-                        labels_train(k) = act;
-                        k=k+1;
-                    end
-                end
-            end
-            
-            %size(X_train);
-            data_train = X_train';
-            model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1 -q' ]);
-            save_svm_model =strcat( './svm_models/run_person', int2str(pe_ts),  '_Ng', Ng,  '_numSPD_', num2str(numSPD), '.mat');
-            save(save_svm_model, 'model');
-            
-        end
-    end
-end
+% scale_factor = 1;
+% shift = 0;
+% load_sub_path =strcat('./FV_training_mulSPD/scale', int2str(scale_factor), '-shift',  int2str(shift));
+% 
+% for i=1:length(vec_Ncent)
+%     Ncent = vec_Ncent(i);
+%     for j=1:length(vec_numSPD)
+%         numSPD = vec_numSPD(j);
+%         Ng = int2str(Ncent);
+%         dim_FV = 2*dim_spdvec*Ncent;
+%         fprintf('Training with Ng: %d - numSPD %d \n',Ncent, numSPD);
+% 
+%         for pe_ts= 1: n_peo
+%             X_train = zeros(dim_FV,n_test);
+%             labels_train = zeros(n_test,1);
+%             k =1;
+%             for pe_tr=1: n_peo
+%                 if pe_tr~=pe_ts
+%                     for act=1: n_actions
+%                         %show_you = strcat(all_people(pe_tr),  '_', actions(act));
+%                         %disp(show_you);
+%                         
+%                         name_load_FV = strcat( load_sub_path, '/FV_', all_people(pe_tr),'_',actions(act),'_sc', sc, '_Ng', Ng,  '_numSPD_', num2str(numSPD), '.txt');
+%                         one_FV = load( char( name_load_FV ) );
+%                         X_train(:,k) = one_FV;
+%                         labels_train(k) = act;
+%                         k=k+1;
+%                     end
+%                 end
+%             end
+%             
+%             %size(X_train);
+%             data_train = X_train';
+%             model = svmtrain(labels_train, data_train, ['-s 0 -t 0 -b 1 -q' ]);
+%             save_svm_model =strcat( './svm_models/run_person', int2str(pe_ts),  '_Ng', Ng,  '_numSPD_', num2str(numSPD), '.mat');
+%             save(save_svm_model, 'model');
+%             
+%         end
+%     end
+% end
 
 %% Testing
 
@@ -104,8 +104,8 @@ all_acc_Ncent_NumSPD= zeros(  length(vec_Ncent), length(vec_numSPD) );
 
 for i=1:length(vec_Ncent)
     Ncent = vec_Ncent(i);
-    for j=1:length(vec_numSPD)
-        numSPD = vec_numSPD(j);
+    for m=1:length(vec_numSPD)
+        numSPD = vec_numSPD(m);
         fprintf('Testing with Ng: %d - numSPD %d \n',Ncent, numSPD);
 
         load_sub_path =strcat('./FV_training_mulSPD/scale', num2str(scale_factor), '-shift',  int2str(shift));
@@ -146,7 +146,7 @@ for i=1:length(vec_Ncent)
         end
         %[acc n_peo n_actions]
         acc = acc*100/(n_peo*n_actions)
-        all_acc_Ncent_NumSPD(i,j) = acc
+        all_acc_Ncent_NumSPD(i,m) = acc
     end
 end
 
