6d8d2512d1b9a41e6d5824ff29e3e9064c799ae8
johanna-codes <johacarvajalg@gmail.com>
2016-02-09 11:23:55 +1000

    Thinking

--- kth/dim_14/FV_SPD/vec_SPD_GMM.cpp
+++ kth/dim_14/FV_SPD/vec_SPD_GMM.cpp
@@ -79,6 +79,8 @@ main(int argc, char** argv)
     for (int i=0; i<vec_num_SPD.n_elem; ++i)
     {
       int num_SPD = vec_num_SPD(i);
+       int num_SPD = vec_num_SPD(i);
+      cout << "num_SPD: " << num_SPD << endl;
       getVecSPD_GMM get_vecs( path, actionNames,  scale, shift, total_scenes, dim );
       get_vecs.get_vecs_video( all_people, num_SPD);
     
